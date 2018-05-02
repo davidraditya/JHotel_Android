aff50d0df13eea9317c19bff68f6a87a0c8ea842
David Raditya K <32066453+davidraditya@users.noreply.github.com>
2018-05-03 01:44:51 +0700

    Test

---
+++ app/src/main/java/com/example/davidraditya/jhotel_android_davidraditya/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.davidraditya.jhotel_android_davidraditya;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final EditText emailInput = (EditText) findViewById(R.id.inputEmail);
+        final EditText passInput = (EditText) findViewById(R.id.inputPass);
+        final Button loginButton = (Button) findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                final String email = emailInput.getText().toString();
+                final String password = passInput.getText().toString();
+
+                if(email.equals("test@test.com") && password.equals("test")) {
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
+                    builder1.setMessage("Login success.")
+                            .create()
+                            .show();
+                }
+                else {
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
+                    builder1.setMessage("Login failed.")
+                            .create()
+                            .show();
+                }
+            }
+        });
+    }
+}
